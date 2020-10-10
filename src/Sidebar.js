411ec64fffac2a9bf31e7de5eba2b170ef7b7ee9
llamousse <vyue013@gmail.com>
2020-10-09 23:48:55 -0700

    initial styling of components

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -2,9 +2,11 @@ import React from "react";
 import "./Sidebar.css";
 
 function Sidebar() {
-  return <div className="sidebar">
+  return (
+    <div className="sidebar">
       <h1>I am sidebar</h1>
-  </div>;
+    </div>
+  );
 }
 
 export default Sidebar;
