1b5fb28908ed4d6a3f7df1c64bec1feb3be0bad6
llamousse <vyue013@gmail.com>
2020-10-11 15:59:58 -0700

    replaced sidebar with material ui icons

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,15 +1,25 @@
 import React from "react";
 import "./Sidebar.css";
+import DonutLargeIcon from "@material-ui/icons/DonutLarge";
+import { Avatar, IconButton } from "@material-ui/core";
+import ChatIcon from "@material-ui/icons/Chat";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
 
 function Sidebar() {
   return (
     <div className="sidebar">
-      <h1>I am sidebar</h1>
       <div className="sidebarHeader">
-          
-          <div className="sidebarHeaderRight">
-
-          </div>
+        <div className="sidebarHeaderRight">
+          <IconButton>
+            <DonutLargeIcon />
+          </IconButton>
+          <IconButton>
+            <ChatIcon />
+          </IconButton>
+          <IconButton>
+            <MoreVertIcon />
+          </IconButton>
+        </div>
       </div>
     </div>
   );
