fb8ae7fc2427f36a9bfbf64d860cd3df47e03467
llamousse <vyue013@gmail.com>
2020-10-19 14:21:38 -0700

    set up connection to mongodb

--- whatsapp-be/server.js
+++ whatsapp-be/server.js
@@ -1,5 +1,6 @@
 // importing
 import express from "express";
+import mongoose from "mongoose"; // client connecting to db
 
 // app config
 const app = express();
@@ -8,6 +9,13 @@ const port = process.env.PORT || 9000;
 // middleware
 
 // DB config
+const connection_url =
+  "mongodb+srv://admin:OEWCMPeCQNevAX5w@cluster0.qyahb.mongodb.net/whatsappdb?retryWrites=true&w=majority";
+mongoose.connect(connection_url, {
+  useCreateIndex: true,
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 // api routes
 app.get("/", (req, res) => res.status(200).send("hello world"));
