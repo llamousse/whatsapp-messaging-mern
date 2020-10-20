4e045dfcb85874e1eca25bdfa1e51b52978c7ee3
llamousse <vyue013@gmail.com>
2020-10-20 16:07:46 -0700

    added api routes for post msg and get all msgs

--- whatsapp-be/server.js
+++ whatsapp-be/server.js
@@ -1,12 +1,14 @@
 // importing
 import express from "express";
 import mongoose from "mongoose"; // client connecting to db
+import Messages from "./dbMessages.js";
 
 // app config
 const app = express();
 const port = process.env.PORT || 9000;
 
 // middleware
+app.use(express.json());
 
 // DB config
 const connection_url =
@@ -19,6 +21,28 @@ mongoose.connect(connection_url, {
 
 // api routes
 app.get("/", (req, res) => res.status(200).send("hello world"));
+
+app.get("/messages/sync", (req, res) => {
+  Messages.find((err, data) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.status(200).send(data);
+    }
+  });
+});
+
+app.post("/messages/new", (req, res) => {
+  const dbMessage = req.body;
+
+  Messages.create(dbMessage, (err, data) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.status(201).send(data);
+    }
+  });
+});
 
 // listener
 app.listen(port, () => console.log(`Listening on localhost:${port}`));
