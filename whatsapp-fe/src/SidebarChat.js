f950d57d45690e0cf458e81157e1b477cd34f8c7
llamousse <vyue013@gmail.com>
2020-10-21 13:42:58 -0700

    added avatar placeholders

--- whatsapp-fe/src/SidebarChat.js
+++ whatsapp-fe/src/SidebarChat.js
@@ -1,17 +1,23 @@
-import { Avatar } from '@material-ui/core'
-import React from 'react'
-import "./SidebarChat.css"
+import React, { useEffect, useState } from "react";
+import { Avatar } from "@material-ui/core";
+import "./SidebarChat.css";
 
 function SidebarChat() {
-    return (
-        <div className="sidebarChat">
-            <Avatar />
-            <div className="sidebarChatInfo">
-                <h2>Room name</h2>
-                <p>This is the last message</p>
-            </div>
-        </div>
-    )
+  const [seed, setSeed] = useState("");
+
+  useEffect(() => {
+    setSeed(Math.floor(Math.random() * 5000));
+  }, []);
+
+  return (
+    <div className="sidebarChat">
+      <Avatar src={`https://avatars.dicebear.com/api/bottts/${seed}.svg`} />
+      <div className="sidebarChatInfo">
+        <h2>Room name</h2>
+        <p>This is the last message</p>
+      </div>
+    </div>
+  );
 }
 
-export default SidebarChat
+export default SidebarChat;
